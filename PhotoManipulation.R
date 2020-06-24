51a5eb04bb73536c34e35c18d520bc7dcc8d7dc5
prestontranbarger <preston.tranbarger@gmail.com>
2020-06-24 14:10:03 -0500

    added features

--- PhotoManipulation.R
+++ PhotoManipulation.R
@@ -3,6 +3,7 @@
 ###############################################
 
 #path <- "//Volumes//GoogleDrive//My Drive//Charlie//PhotoMappingProjectDevelopment//Data//Photos//"
+
 check.photo.format <- function(filename){
   ##########################################
   # Verify that the file is an image file  #
@@ -10,25 +11,20 @@ check.photo.format <- function(filename){
   
   t.strsplit <- strsplit(filename, ".", fixed=T)
   suffix <- t.strsplit[[1]][2]
-  
   if (!is.na(suffix) & ((suffix=="JPG") | (suffix =="jpg") | (suffix == "HEIC"))) {
     output <- TRUE
   } else output <- FALSE
-  ##########################################
-  # Eventually could do virus checks here  #
-  ##########################################
+  #TODO: possible virus checks?
   output
 }
+
 extract.exif.info <- function(filename){
   ##########################################
   # Extract the pieces of information from #
   # EXIF header.                           #
   ##########################################
+  
   t.exif <- read_exif(filename)
-
-  ##########################################
-  # Check to see if geotagged              #
-  ##########################################  
   t.exif.names <- names(t.exif)
   t.match <- match(c("GPSLatitude", "GPSLongitude"), t.exif.names)
   if(length(t.match)==2){
@@ -36,26 +32,26 @@ extract.exif.info <- function(filename){
       geotagged.flag <- TRUE
     else geotagged.flag <- FALSE
   } else geotagged.flag <- FALSE
-  
-  ##########################################
-  # Process geotagged data                 #
-  ##########################################    
   if(geotagged.flag){
     output <- t.exif %>%
       select("GPSLatitude", "GPSLongitude", "GPSAltitude", "GPSDateTime")
   } else output <- NULL
   output
 }
+
 readPhotoCoordinates <- function(path){
+  ##########################################
+  # Creates a matrix with containing exif  #
+  # data of all the image files in the     #
+  # folder.                                #
+  ##########################################
+  
   t.dir <- dir(path)
   t.dir.list <- as.list(t.dir)
   t.checked <- unlist(lapply(t.dir, FUN=check.photo.format))
   t.dir <- t.dir[t.checked]
   t.dir <- paste0(path, t.dir)
   t.photo.coordinates <- lapply(t.dir, FUN=extract.exif.info)
-  ###############################################
-  # Remove any elements that were not geocoded  #
-  ###############################################
   t.null <- unlist(lapply(t.photo.coordinates, FUN=is.null))
   t.photo.coordinates <- t.photo.coordinates[!t.null]
   for(i in 1:length(t.photo.coordinates)){
@@ -75,7 +71,30 @@ readPhotoCoordinates <- function(path){
     if(t.names[i]=="GPSLongitude") t.matrix[,i] <- as.numeric(t.matrix[,i])
     if(t.names[i]=="GPSAltitude") t.matrix[,i] <- as.numeric(t.matrix[,i])
   }
+  t.matrix$Name <- readPhotoNames(path)
   t.matrix
 }
 
+readPhotoNames <- function(path){
+  ##########################################
+  # Produces a list of file names ending   #
+  # in '.jpg', '.JPG', or '.HEIC'.         #
+  ##########################################
+  
+  list.files(path, pattern = '((.*)(?i)\\.jpg)|(.*)\\.HEIC')
+}
 
+photosInsideBoundingBox <- function(photos){
+  ##########################################
+  # Produces a list of all photos          #
+  # contained within the confines of the   #
+  # boudning box.                          #
+  ##########################################
+  
+  photosIn <- photos %>%
+    filter(GPSLongitude > globalValues$b.box[1] & GPSLongitude < globalValues$b.box[3])
+  photosIn <- photosIn %>%
+    filter(GPSLatitude > globalValues$b.box[2] & GPSLatitude < globalValues$b.box[4])
+  names <- photosIn$Name
+  names
+}
--- server.R
+++ server.R
@@ -3,54 +3,197 @@
 # You can find out more about building applications with Shiny here:
 #
 # http://shiny.rstudio.com
-#
 
 library(shiny)
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
 
-  photos <- readPhotoCoordinates(path=".//Data//Photos//")
-  observeEvent(input$plot_brush, {
-    ##################################
-    # Create a new bounding box      #
-    ##################################
-    # columns are min & max
-    # rows are x & y
-    print(input$plot_brush$xmin)
-    t.matrix <- matrix(nrow=2, ncol=2)
-    t.matrix[1,] <- c(input$plot_brush$xmin, input$plot_brush$xmax)
-    t.matrix[2,] <- c(input$plot_brush$ymin, input$plot_brush$ymax)
-    row.names(t.matrix) <- c("x", "y")
-    colnames(t.matrix) <- c("min", "max")
-    globalValues$b.box <- t.matrix
-  })
+  photos <- readPhotoCoordinates(directoryPath)
   
   output$theMap <- renderPlot({
-    t.sampled.photos <- sample.photos(photos, input$photoNumber)
-    t.box <- create.bounding.box(t.sampled.photos)
-    globalValues$b.box <- t.box
-    print(t.box)
+    ##################################
+    # Draws map and creates a        #
+    # bounding box with 2.5% margins #
+    # if system is not in user       #
+    # defined map control mode.      #
+    ##################################
     
+    t.sampled.photos <- sample.photos.select(photos, input$checkedPhotos)
+    if(!input$userControlMap){
+      t.box <- create.bounding.box(t.sampled.photos, 2.5)
+      globalValues$b.box <- t.box
+    }
     create.map(t.sampled.photos, bounding.box=globalValues$b.box)
-    #create.map(t.sampled.photos, bounding.box=t.box)
   })
+  
   output$info <- renderText({
+    ##################################
+    # Makes text on stats at the     #
+    # bottom of the window work.     #
+    ##################################
+    
     xy_str <- function(e) {
       if(is.null(e)) return("NULL\n")
-      paste0("x=", round(e$x, 1), " y=", round(e$y, 1), "\n")
-    }
-    xy_range_str <- function(e) {
-      if(is.null(e)) return("NULL\n")
-      paste0("xmin=", round(e$xmin, 1), " xmax=", round(e$xmax, 1), 
-             " ymin=", round(e$ymin, 1), " ymax=", round(e$ymax, 1))
+      paste0("x=", round(e$x, 2), " y=", round(e$y, 2), "\n")
     }
     
+    dms_str <- function(e) {
+      if(is.null(e)) return("NULL\n")
+      
+      if(e$y < 0){
+        yChar <- "S"
+      } else {
+        yChar <- "N"
+      }
+      
+      if(e$x < 0){
+        xChar <- "W"
+      } else {
+        xChar <- "E"
+      }
+      
+      x <- abs(e$x)
+      dx <- floor(x)
+      mx <- floor((x - dx) * 60)
+      sx <- floor((x - dx - mx / 60) * 3600)
+      y <- abs(e$y)
+      dy <- floor(y)
+      my <- floor((y - dy) * 60)
+      sy <- floor((y - dy - my / 60) * 3600)
+      
+      paste0(dy, "°", my, "'", sy, "\"", yChar, " ", dx, "°", mx, "'", sx, "\"", xChar, "\n")
+    }
+    
+    xy_range_str <- function(e) {
+      if(is.null(e)) return("NULL\n")
+      paste0("xmin=", round(e$xmin, 2), " xmax=", round(e$xmax, 2), 
+             " ymin=", round(e$ymin, 2), " ymax=", round(e$ymax, 2), "\n")
+    }
+    
+    str_out <- function(e) {
+      if(is.null(e)) return("NULL\n")
+      paste0(e, "\n")
+    }
+    
+    selectedPhotos <- filter(photos, Name %in% input$checkedPhotos)
+    
     paste0(
-      "click: ", xy_str(input$plot_click),
-      "dblclick: ", xy_str(input$plot_dblclick),
-      "hover: ", xy_str(input$plot_hover),
-      "brush: ", xy_range_str(input$plot_brush)
+      c(#" click:", xy_str(input$plot_click),
+      #"dblclick:", xy_str(input$plot_dblclick),
+      " hover:", dms_str(input$plot_hover),
+      #"brush:", xy_range_str(input$plot_brush),
+      "selected photo:", str_out(globalValues$selectedPhoto),
+      "selected photos:", photosInsideBoundingBox(selectedPhotos),
+      "\n photos on map:", photosInsideBoundingBox(photos))
     )
   })
-
+  
+  output$photoDistTable <- renderTable(globalValues$photoDistTable,
+                                       options = list(pageLength = 5))
+  
+  observeEvent(input$plot_dblclick, {
+    ##################################
+    # Create a new bounding box      #
+    # when mouse is double clicked   #
+    # using brush coordinates if the #
+    # brush is not null. If the      #
+    # brush is null then a routine   #
+    # begins to find the nearest     #
+    # photo and select it.           #
+    ##################################
+    
+    if(!is.null(input$plot_brush)){
+      t.matrix <- matrix(nrow=2, ncol=2)
+      t.matrix[1,] <- c(input$plot_brush$xmin, input$plot_brush$xmax)
+      t.matrix[2,] <- c(input$plot_brush$ymin, input$plot_brush$ymax)
+      row.names(t.matrix) <- c("x", "y")
+      colnames(t.matrix) <- c("min", "max")
+      globalValues$b.box <- t.matrix
+      session$resetBrush("plot_brush")
+      
+      ##################################
+      # Deselects photos outside of    #
+      # a user defined bounding box.   #
+      ##################################
+      
+      selectedPhotos <- filter(photos, Name %in% input$checkedPhotos)
+      updateCheckboxGroupInput(session,
+                               "checkedPhotos",
+                               choices = readPhotoNames(directoryPath),
+                               selected = photosInsideBoundingBox(selectedPhotos))
+    } else {
+      photosInside <- photosInsideBoundingBox(photos)
+      photosIn <- photos %>%
+        filter(Name %in% photosInside)
+      photosIn$distance <- calc.distance(input$plot_dblclick, photosIn)
+      photosIn <- arrange(photosIn, distance)
+      photosIn <- head(photosIn, 1)
+      globalValues$selectedPhoto <- photosIn$Name
+    }
+  })
+  
+  observeEvent(input$selectAll, {
+    ##################################
+    # Makes select all checkbox      #
+    # function correctly.            #
+    ##################################
+    
+    if(input$userControlMap){
+      updateCheckboxGroupInput(session,
+                               "checkedPhotos",
+                               choices = readPhotoNames(directoryPath),
+                               selected = if(input$selectAll) photosInsideBoundingBox(photos))
+    } else {
+      updateCheckboxGroupInput(session,
+                               "checkedPhotos",
+                               choices = readPhotoNames(directoryPath),
+                               selected = if(input$selectAll) readPhotoNames(directoryPath))
+    }
+  })
+  
+  observe({
+    ##################################
+    # Deselects the selected photo   #
+    # if selected photo is outside   #
+    # the bounding box or is         #
+    # deselected.                    #
+    ##################################
+    
+    selectedPhotos <- filter(photos, Name %in% input$checkedPhotos)
+    
+    if(!is.null(globalValues$selectedPhoto)){
+      if(!is.null(photosInsideBoundingBox(photos))){
+        if(!(globalValues$selectedPhoto %in% photosInsideBoundingBox(photos))){
+          globalValues$selectedPhoto <- NULL
+        }
+        if(!is.null(selectedPhotos)){
+          if(!(globalValues$selectedPhoto %in% selectedPhotos$Name)){
+            globalValues$selectedPhoto <- NULL
+          }
+        }
+      }
+    }
+  })
+  
+  observe({
+    ##################################
+    # Updates table for photo        #
+    # distances.                     #
+    ##################################
+    
+    if(!is.null(globalValues$selectedPhoto)){
+      selectedPhoto <- filter(photos, Name %in% globalValues$selectedPhoto)
+      selectedPhotos <- filter(photos, Name %in% input$checkedPhotos)
+      fakeClick <- selectedPhoto %>%
+        mutate(x = GPSLongitude, y = GPSLatitude)
+      photosInsideNames <- photosInsideBoundingBox(selectedPhotos)
+      photosInside <- filter(photos, Name %in% photosInsideNames)
+      photosInside$Distance <- calc.distance(fakeClick, photosInside)
+      photosInside <- arrange(photosInside, Distance)
+      photosInside <- subset(photosInside, select = -c(GPSAltitude, GPSDateTime))
+      globalValues$photoDistTable <- photosInside
+    } else {
+      globalValues$photoDistTable <- NULL
+    }
+  })
 })
